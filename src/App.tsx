fd4e5f41a8986ff98fc85381d0868d3917f234a2
manoel <manoeldiasmf@outlook.com>
2023-02-06 19:11:47 -0300

    Styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import styled from "styled-components"
+
+const Title = styled.h1`
+  font-size: 65px;
+  color: plum;
+`
 
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+    <Title>Hello World</Title>
     </div>
   );
 }
