30a5e40fb3acb344933b90f45f63abbd30fae51b
manoel <manoeldiasmf@outlook.com>
2023-02-06 21:48:03 -0300

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 :root {
-  --background: #f0f2f5
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+    --green: #33cc95;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --background: #f0f2f5;
+    --shape: #ffffff
 }
 
 
@@ -26,6 +37,23 @@ html {
 body {
   background: var(--background);
   -webkit-font-smoothing: antialiased;
+}
+body, input, textarea, button {
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+  font-weight: 600;
+}
+
+button{
+  cursor: pointer;
+}
+
+[disabled]{
+  opacity: 0.6;
+  cursor: not-allowed;
 }
 
 `
