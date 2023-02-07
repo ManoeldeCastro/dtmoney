63a4fb85ba9e1b7a35d6cbb46b7d4f8784f7b4dc
manoel <manoeldiasmf@outlook.com>
2023-02-07 17:28:05 -0300

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -10,7 +10,7 @@ export const Content = styled.div`
   max-width: 1120px;
   margin: 0 auto;
   
-  padding: 0 1rem 10rem;
+  padding: 2rem 1rem 12rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -21,6 +21,15 @@ export const Content = styled.div`
     color: #fff;
     background: var(--blue-light);
     border: 0;
-    
+    padding: 0 2rem;
+    border-radius: 0.25rem ;
+    height: 3rem;
+
+
+    transition: filter 0.3s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
   }
 `
