6044bcd82377e3c251cd2fb2a45e2ad282e77f83
manoel <manoeldiasmf@outlook.com>
2023-02-08 18:22:07 -0300

    table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -17,25 +17,17 @@ return(
           <td>
             Desenvolvimento de Website
           </td>
-          <td>R$12.000</td>
+          <td className="deposit">R$12.000</td>
           <td>Desenvolvimento</td>
-          <td>07-02-2023</td>
+          <td>07/02/2023</td>
         </tr>
         <tr>
           <td>
-            Desenvolvimento de Website
+            Aluguel
           </td>
-          <td>R$12.000</td>
-          <td>Desenvolvimento</td>
-          <td>07-02-2023</td>
-        </tr>
-        <tr>
-          <td>
-            Desenvolvimento de Website
-          </td>
-          <td>R$12.000</td>
-          <td>Desenvolvimento</td>
-          <td>07-02-2023</td>
+          <td className="withdraw">-R$1.600</td>
+          <td>Casa</td>
+          <td>16/02/2023</td>
         </tr>
         
       </tbody>
